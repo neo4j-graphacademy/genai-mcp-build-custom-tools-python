b08bbfe3e2baa0dac78a66fdf2fd2c01802cae53
Adam Cowley <adam@adamcowley.co.uk>
2025-10-23 13:03:48 +0100

    install dependencies for client

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -9,3 +9,9 @@ curl https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 \. "$NVM_DIR/nvm.sh"
 
 nvm install 22
+
+# Install client dependencies
+cd client
+uv venv
+source .venv/bin/activate
+uv pip install -e .
